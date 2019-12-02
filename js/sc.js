413ab222094d01cad94198da6eed920aee83eaea
luiz-stein <56131141+luiz-stein@users.noreply.github.com>
2019-12-02 18:42:13 -0300

    Add files via upload

--- js/sc.js
+++ js/sc.js
@@ -1,1 +1,13 @@
-//ss
+//funcoes javascript e integracao solidity
+
+function enviarGarantiaVenda()
+{
+    document.getElementById('status_garantia_venda').innerHTML = "<b>Sim, transferido 2 ethers</b>";;
+    document.getElementById('id-garantia-vendedor').style.display = 'none'
+}
+
+function enviarGarantiaCompra()
+{
+    document.getElementById('status_garantia_compra').innerHTML = "<b>Sim, transferido 2 ethers</b>";;
+    document.getElementById('id-garantia-comprador').style.display = 'none'
+}
