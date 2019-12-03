56c6c6a9db1700a57f0c737dd5268edf40570e0e
luiz-stein <56131141+luiz-stein@users.noreply.github.com>
2019-12-03 16:58:24 -0300

    Add files via upload

--- js/blockchain.js
+++ js/blockchain.js
@@ -3,63 +3,94 @@ var enderecoContrato = "0xaA84710Cb64e37719c86Cb6dD8f44584c6A75a49";
 var provider = new ethers.providers.Web3Provider(web3.currentProvider);
 var signer = provider.getSigner();
 var contract = new ethers.Contract(enderecoContrato, contractEscrow, signer);
+ethereum.enable();
 
 function enviarGarantiaVenda()
 {
-    document.getElementById('status_garantia_venda').innerHTML = "<b>Sim, transferido 2 gwei</b>";;
-    document.getElementById('id-garantia-vendedor').style.display = 'none'
-}
-
-function getContractBalance() {    
-    
-    console.log("getContractBalance - submitting the request");     
-    contractSaldoDoContrato()
-    .then( (resultFromContract) => {
-        console.log("getContractBalance - result is", resultFromContract);
+    document.getElementById('status_garantia_venda').innerHTML = "<b>Transacao enviada ....</b>";;
+        var additionalSettings = {
+            value: ethers.utils.parseUnits("200000000", 'wei')
+        }; 
         
-    })
+        contract.DepositarGarantiaVendedor(additionalSettings)
+        .then( (tx) => {
+            console.log("executePayment - Transaction ", tx);   
+    document.getElementById('status_garantia_venda').innerHTML = "<b>Aguardando resutado ...</b>";;
+        tx.wait()
+        .then( (resultFromContract) => {
+            console.log("executePayment - the result was ", resultFromContract);
+    document.getElementById('status_garantia_venda').innerHTML = "<b>Valor transferido da Garantia</b>";
+    document.getElementById('id-garantia-vendedor').style.display = 'none';
+    })        
     .catch( (err) => {
-        console.error(err);
-        alert("A screen will be load asking to allow this page to connect with your Ethereum account.\nPlease give this permission to proceed.\nOr if you don't have an Ethereum account please install Metamask");
-        ethereum.enable();
-        alert("After you give the permission we are going to reload the page");
-        document.location = "index.html";
-    });
+        console.error("executePayment - after tx being mint");
+    console.error(err);
+    document.getElementById('status_garantia_venda').innerHTML = "<b>Erro ao transferir a Garantia</b>" +" "+ err.message;
+    document.getElementById('id-garantia-vendedor').style.display = 'none';
+    })
+    })
+        .catch( (err) => {
+         console.error("executePayment - tx has been sent");
+         console.error(err);
+    document.getElementById('status_garantia_venda').innerHTML = "<b>Erro ao transferir a Garantia</b>" + err.message;
+    document.getElementById('id-garantia-vendedor').style.display = 'none';
+    })
+    
 }
 
 function enviarGarantiaCompra()
 {
-    var additionalSettings = 
-        {
-            value: "200000000"
+    document.getElementById('status_garantia_compra').innerHTML = "<b>Transacao enviada ....</b>";;
+        var additionalSettings = {
+            value: ethers.utils.parseUnits("200000000", 'wei')
         }; 
-    contract.DepositarGarantiaComprador(additionalSettings)
-        .then( (tx) => 
-         {
-                console.log("executePayment - Transaction ", tx);   
-                document.getElementById('status_garantia_compra').innerHTML = "Aguardando a transação de garantia do comprador ...";
-                tx.wait()
-                .then( (resultFromContract) => 
-                {
-                    console.log("executePayment - the result was ", resultFromContract);
-                    document.getElementById('status_garantia_compra').innerHTML = "<b>Sim, transferido 2 gwei</b>";
-                    document.getElementById('id-garantia-comprador').style.display = 'none';
-                })        
-                .catch( (err) => 
-                {
-                    console.error("executePayment - after tx being mint");
-                    console.error(err);
-                    document.getElementById('status_garantia_compra').innerHTML = "<b>Erro. Garantia não transferida</b>";
-                    document.getElementById('id-garantia-comprador').style.display = 'none';
-                })
-         })
-       .catch( (err) => {
-                console.error("executePayment - tx has been sent");
-                console.error(err);
-                document.getElementById('status_garantia_compra').innerHTML = "<b>Erro. Garantia não transferida</b>";
-                document.getElementById('id-garantia-comprador').style.display = 'none';
-       })
+    
+        contract.DepositarGarantiaVendedor(additionalSettings)
+        .then( (tx) => {
+            console.log("executePayment - Transaction ", tx);   
+    document.getElementById('status_garantia_compra').innerHTML = "<b>Aguardando resutado ...</b>";;
+        tx.wait()
+        .then( (resultFromContract) => {
+            console.log("executePayment - the result was ", resultFromContract);
+    document.getElementById('status_garantia_compra').innerHTML = "<b>Valor transferido da Garantia</b>";
+    document.getElementById('id-garantia-comprador').style.display = 'none';
+    })        
+    .catch( (err) => {
+        console.error("executePayment - after tx being mint");
+    console.error(err);
+    document.getElementById('status_garantia_compra').innerHTML = "<b>Erro ao transferir a Garantia</b>" + err.message;
+    document.getElementById('id-garantia-comprador').style.display = 'none';
+    })
+    })
+        .catch( (err) => {
+         console.error("executePayment - tx has been sent");
+         console.error(err);
+    document.getElementById('status_garantia_compra').innerHTML = "<b>Erro ao transferir a Garantia</b>" + err.message;
+    document.getElementById('id-garantia-comprador').style.display = 'none';
+    })
 }
+
+function encerrarContrato()
+{
+   console.log("EncerrarCompra - submitting the request");     
+   contract.EncerrarCompra()
+   .then( (resultFromContract) => {
+     console.log("encerrarcompra- result is", resultFromContract);
+     document.getElementById('status_encerrado').innerHTML = "<b>Contato Encerrado</b>";
+   
+})
+.catch( (err) => {
+    console.error(err);
+    alert("A screen will be load asking to allow this page to connect with your Ethereum account.\nPlease give this permission to proceed.\nOr if you don't have an Ethereum account please install Metamask");
+    ethereum.enable();
+    alert("After you give the permission we are going to reload the page");
+    document.getElementById('status_encerrado').innerHTML = "<b>Contato Encerrado</b>" + err.message;
+    document.location = "index.html";
+});
+}
+
+
+
 
 //function executePayment() 
 //{
