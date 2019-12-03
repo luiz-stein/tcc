a7a754e482f49938d8798eefe3d3c1c5d9fa9499
luiz-stein <56131141+luiz-stein@users.noreply.github.com>
2019-12-03 14:07:58 -0300

    Update sc.js

--- js/sc.js
+++ js/sc.js
@@ -1,5 +1,5 @@
 //funcoes javascript e integracao solidity
-
+var enderecoContrato ="0xaA84710Cb64e37719c86Cb6dD8f44584c6A75a49";
 function enviarGarantiaVenda()
 {
     document.getElementById('status_garantia_venda').innerHTML = "<b>Sim, transferido 2 ethers</b>";;
